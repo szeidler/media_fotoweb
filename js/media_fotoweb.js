cb1bd5ccddd09a98093c21a373cd7055f739b8de
Stephan Zeidler <stephan@ramsalt.com>
2017-02-23 19:05:55 +0100

    Add file object creation methods on widget selection

--- js/media_fotoweb.js
+++ js/media_fotoweb.js
@@ -2,22 +2,34 @@
 
   Drupal.behaviors.media_fotoweb_selection_listener = {
     attach: function (context, settings) {
-      function listener(event) {
-        var url = 'https://fotoweb.nsf.no';
+      function fotowebSelectionlistener(event) {
+        var url = settings.media_fotoweb.fotoweb_host;
+
+        // Listen for messages only from the Fotoweb API.
         if (event.origin != url) {
           return;
         }
+
+        // Post the retrieved asset data with an AJAX request.
         var data = event.data.asset;
         if (event.data.event === 'assetSelected') {
-          console.log(data);
+          $.ajax({
+            url: Drupal.settings.media_fotoweb.assetAddAjaxUrl,
+            type: 'POST',
+            data: {asset: data},
+            dataType: '',
+            success: function (data) {
+              $('#ajax-result').html(data);
+            }
+          });
         }
       }
 
       if (window.addEventListener) {
-        addEventListener('message', listener, false);
+        addEventListener('message', fotowebSelectionlistener, false);
       }
       else {
-        attachEvent('onmessage', listener);
+        attachEvent('onmessage', fotowebSelectionlistener);
       }
     }
   };
