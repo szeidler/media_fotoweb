13650dbeb87b6a7dde9c5281b444640d897785a4
Stephan Zeidler <stephan@ramsalt.com>
2017-08-12 01:33:08 +0200

    let's try a after

---
+++ tests/drupal_ti/after/runners/simpletest/before_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Add an optional statement to see that this is running in Travis CI.
+echo "running drupal_ti/before/before_script.sh"
+
+set -e $DRUPAL_TI_DEBUG
+
+# Ensure the right Drupal version is installed.
+# The first time this is run, it will install Drupal.
+# Note: This function is re-entrant.
+drupal_ti_ensure_drupal
+
+# Change to the Drupal directory
+cd "$DRUPAL_TI_DRUPAL_DIR"
+
+# Create the the module directory (only necessary for D7)
+# For D7, this is sites/default/modules
+# For D8, this is modules
+mkdir -p "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+
+# Enable composer module
+echo "DRUPAL TI - Download Composer module and enable"
+drush dl composer-8.x-1.x
+echo "DRUPAL TI - Clear Drush cache"
+drush cc drush
+drush cc all
+echo "DRUPAL TI - Delete cache dir"
+rm -f "$DRUPAL_TI_CACHE_DIR"/HOME/.drush/cache
+
+# Download required modules.
+drush dl composer_manager
+drush en -y composer_manager
+
+drush composer-json-rebuild
+
+# Ensure the module is linked into the code base and enabled.
+# Note: This function is re-entrant.
+drupal_ti_ensure_module_linked
